1b78569c5a5c5351cca1e53c4b4cf873ec0fdfb3
Rasmus Björling <bjorling.r@gmail.com>
2025-04-13 15:27:11 +0200

    Added Unit Tests

---
+++ Tests/UnitTests.cs
@@ -0,0 +1,72 @@
+using Core.Logic;
+using Core.Models;
+
+namespace Tests
+{
+    public class UnitTests
+    {
+        [Fact]
+        public void Test_TimeGrouping()
+        {
+            DateTime now = DateTime.Now.Date;
+
+            Assert.True(now.GetTimeGrouping().Equals(TimeGrouping.Night_18to06));
+            Assert.True(now.AddHours(6).AddTicks(-1).GetTimeGrouping().Equals(TimeGrouping.Night_18to06));
+
+            Assert.True(now.AddHours(6).GetTimeGrouping().Equals(TimeGrouping.Morning_06to12));
+            Assert.True(now.AddHours(12).AddTicks(-1).GetTimeGrouping().Equals(TimeGrouping.Morning_06to12));
+
+            Assert.True(now.AddHours(12).GetTimeGrouping().Equals(TimeGrouping.Afternoon_12to18));
+            Assert.True(now.AddHours(18).AddTicks(-1).GetTimeGrouping().Equals(TimeGrouping.Afternoon_12to18));
+
+            Assert.True(now.AddHours(18).GetTimeGrouping().Equals(TimeGrouping.Night_18to06));
+            Assert.True(now.AddHours(18 + 12).AddTicks(-1).GetTimeGrouping().Equals(TimeGrouping.Night_18to06));
+        }
+
+        [Fact]
+        public void Test_Averages()
+        {
+            HourlyData hourly = new HourlyData();
+
+            List<int> nums = new List<int>();
+
+            double mBase = 0, aBase = 0, nBase = 0;
+
+            for (int i = 0; i < 24; i++)
+            {
+                nums.Add(i);
+
+                if (i < 6)
+                {
+                    nBase += i;
+                }
+                else if (i < 12)
+                {
+                    mBase += i;
+                }
+                else if (i < 18)
+                {
+                    aBase += i;
+                }
+                else // >= 18
+                {
+                    nBase += i;
+                }
+            }
+
+            hourly.Hourly.Time = nums.Select(i => $"{DateTime.Now.Date.AddHours(i):yyyy-MM-dd HH:mm}").ToArray();
+            hourly.Hourly.Pm2pt5 = nums.Select(i => i * 2.0).ToArray();
+            hourly.Hourly.Pm10 = nums.Select(i => i * 17.0).ToArray();
+
+            var avgs = AveragesLogic.GetAvgPMValues(hourly);
+
+            Assert.True(avgs.Pm2pt5.Morning == mBase * 2 / 6);
+            Assert.True(avgs.Pm2pt5.Afternoon == aBase * 2 / 6);
+            Assert.True(avgs.Pm2pt5.Night == nBase * 2 / 12);
+
+            Assert.True(avgs.Pm10.Morning == mBase * 17 / 6);
+            Assert.True(avgs.Pm10.Afternoon == aBase * 17 / 6);
+            Assert.True(avgs.Pm10.Night == nBase * 17 / 12);
+        }
+    }
+}
